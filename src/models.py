b7dd3ebec0b5d2256a6f51707451a490f700ef9c
JayThibs <thibo.jacques@gmail.com>
2021-07-25 22:45:45 -0300

    Added comments.

--- src/models.py
+++ src/models.py
@@ -1,11 +1,11 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List  # enforce List type from request
+from pydantic import BaseModel  # enforce typing
 
 
 class Content(BaseModel):
-    post_url: str
-    content: str
+    post_url: str  # posted url needs to be string
+    content: str  # received content needs to be string
 
 
 class Payload(BaseModel):
-    data: List[Content]
+    data: List[Content]  # received data needs to be list of Content
